96005c12e43d6550093f877eaef646c49a4d7df2
okjack <okjackcaptain@gmail.com>
2024-10-23 15:47:12 +0800

    update for compile

--- android/contrib/tools/do-compile-libuavs3d.sh
+++ android/contrib/tools/do-compile-libuavs3d.sh
@@ -102,31 +102,7 @@ mkdir -p $FF_PREFIX
 # mkdir -p $FF_SYSROOT
 
 #--------------------
-echo ""
-echo "--------------------"
-echo "[*] make NDK standalone toolchain"
-echo "--------------------"
 . ./tools/do-detect-env.sh
-FF_MAKE_TOOLCHAIN_FLAGS=$IJK_MAKE_TOOLCHAIN_FLAGS
-FF_MAKE_FLAGS=$IJK_MAKE_FLAG
-
-
-FF_MAKE_TOOLCHAIN_FLAGS="$FF_MAKE_TOOLCHAIN_FLAGS --install-dir=$FF_TOOLCHAIN_PATH"
-FF_TOOLCHAIN_TOUCH="$FF_TOOLCHAIN_PATH/touch"
-if [ ! -f "$FF_TOOLCHAIN_TOUCH" ]; then
-    $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
-        $FF_MAKE_TOOLCHAIN_FLAGS \
-        --platform=$FF_ANDROID_PLATFORM \
-        --toolchain=$FF_TOOLCHAIN_NAME
-    touch $FF_TOOLCHAIN_TOUCH;
-fi
-
-#--------------------
-echo ""
-echo "--------------------"
-echo "[*] check libuavs3d env"
-echo "--------------------"
-export PATH=$FF_TOOLCHAIN_PATH/bin:$PATH
 
 
 cd $FF_SOURCE

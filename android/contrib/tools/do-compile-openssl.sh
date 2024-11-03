fc460f38bca8df8b616685f73ca952991b26aaca
okjack <okjackcaptain@gmail.com>
2024-11-03 21:08:58 +0800

    update ndk19

--- android/contrib/tools/do-compile-openssl.sh
+++ android/contrib/tools/do-compile-openssl.sh
@@ -138,16 +138,55 @@ echo "--------------------"
 . ./tools/do-detect-env.sh
 FF_MAKE_TOOLCHAIN_FLAGS=$IJK_MAKE_TOOLCHAIN_FLAGS
 FF_MAKE_FLAGS=$IJK_MAKE_FLAG
+FF_CC=$IJK_CC
 
 
-FF_MAKE_TOOLCHAIN_FLAGS="$FF_MAKE_TOOLCHAIN_FLAGS --install-dir=$FF_TOOLCHAIN_PATH"
-FF_TOOLCHAIN_TOUCH="$FF_TOOLCHAIN_PATH/touch"
-if [ ! -f "$FF_TOOLCHAIN_TOUCH" ]; then
-    $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
-        $FF_MAKE_TOOLCHAIN_FLAGS \
-        --platform=$FF_ANDROID_PLATFORM \
-        --toolchain=$FF_TOOLCHAIN_NAME
-    touch $FF_TOOLCHAIN_TOUCH;
+if [ -d "$FF_TOOLCHAIN_PATH" ]; then
+    rm -rf $FF_TOOLCHAIN_PATH
+fi
+if [ "$FF_CC" = "gcc" ]; then
+    if [ ! -f "$FF_TOOLCHAIN_TOUCH" ]; then
+        $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
+            $FF_MAKE_TOOLCHAIN_FLAGS \
+            --platform=android-$FF_ANDROID_PLATFORM \
+            --toolchain=$FF_TOOLCHAIN_NAME
+    fi
+else
+    ARCH=$FF_ARCH
+    if [ "$FF_ARCH" = "armv7a" ]; then
+        ARCH=arm
+    fi
+    FF_MAKE_TOOLCHAIN_FLAGS="--install-dir $FF_TOOLCHAIN_PATH --arch $ARCH --api $FF_ANDROID_API"
+    python3 $ANDROID_NDK/build/tools/make_standalone_toolchain.py \
+        $FF_MAKE_TOOLCHAIN_FLAGS
+fi
+
+#--------------------
+echo ""
+echo "--------------------"
+echo "[*] check ffmpeg env"
+echo "--------------------"
+FF_TOOLCHAIN_PATH_BIN=$FF_TOOLCHAIN_PATH/bin
+export PATH=$FF_TOOLCHAIN_PATH_BIN:$PATH
+#export CC="ccache ${FF_CROSS_PREFIX}-gcc"
+export CC=${FF_TOOLCHAIN_PATH_BIN}/${FF_CROSS_PREFIX}-${FF_CC}
+export LD=${FF_TOOLCHAIN_PATH_BIN}/${FF_CROSS_PREFIX}-ld
+export AR=${FF_TOOLCHAIN_PATH_BIN}/${FF_CROSS_PREFIX}-ar
+export STRIP=${FF_TOOLCHAIN_PATH_BIN}/${FF_CROSS_PREFIX}-strip
+
+NM=${FF_TOOLCHAIN_PATH_BIN}/${FF_CROSS_PREFIX}-nm
+RANLIB=${FF_TOOLCHAIN_PATH_BIN}/${FF_CROSS_PREFIX}-ranlib
+if [ ! -f "$AR" ]; then
+    ln -s ${FF_TOOLCHAIN_PATH_BIN}/llvm-ar $AR
+fi
+if [ ! -f "$STRIP" ]; then
+    ln -s ${FF_TOOLCHAIN_PATH_BIN}/llvm-strip $STRIP
+fi
+if [ ! -f "$NM" ]; then
+    ln -s ${FF_TOOLCHAIN_PATH_BIN}/llvm-nm $NM
+fi
+if [ ! -f "$RANLIB" ]; then
+    ln -s ${FF_TOOLCHAIN_PATH_BIN}/llvm-ranlib $RANLIB
 fi
 
 

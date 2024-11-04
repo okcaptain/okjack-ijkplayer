7041d364f81f501f7b38ad93bf7f59f2ebf5f094
okjack <okjackcaptain@gmail.com>
2024-11-04 13:19:18 +0800

    update ndk19

--- android/ijkplayer/ijkplayer-arm64/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-arm64/src/main/jni/Application.mk
@@ -20,7 +20,6 @@
 APP_OPTIM := release
 APP_PLATFORM := android-21
 APP_ABI := arm64-v8a
-NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
 APP_STL := c++_shared
--- android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
@@ -20,7 +20,6 @@
 APP_OPTIM := release
 APP_PLATFORM := android-16
 APP_ABI := armeabi-v7a
-NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
 APP_STL := c++_shared

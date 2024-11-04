3eeb42c0be4f1578111d69659674aabfd5a2a636
okjack <okjackcaptain@gmail.com>
2024-11-04 13:04:07 +0800

    update ndk19

--- android/ijkplayer/ijkplayer-arm64/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-arm64/src/main/jni/Application.mk
@@ -28,5 +28,4 @@ APP_STL := stlport_static
 APP_CFLAGS := -O3 -Wall -pipe \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
     -DANDROID -DNDEBUG
